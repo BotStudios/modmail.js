5652b6a1ad300018920e41c0932022cfc6407eb1
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-06-10 14:26:25 +0800

    Delete index.js

--- modmail.js/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-exports.on = require('./mail.js')
-exports.inbox = require('./lib/client/inboxMail.js')
-exports.prefix = require('./lib/client/prefix.js')
-exports.perms = require('./lib/client/perms.js')
