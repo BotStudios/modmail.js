0f24c491fce540a522444d33e3b654b949b61484
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-06-21 00:16:08 +0800

    Create index.js

---
+++ example/index.js
@@ -0,0 +1,19 @@
+const modmail = require('modmail.js') 
+
+modmail.on({ 
+  token: 'BOT_TOKEN', 
+  inbox: 'INBOX_CHANNEL_ID', 
+  prefix: 'BOT_PREFIX', 
+  message: '@mentions/messages', 
+  inline: true, // inlineReply
+  
+  plugins: [{ // plugins : https://modmail.js.org/plugins
+     replyToReply: true, // replyToReply : https://modmail.js.org/plugins/replyToReply
+     cache: './FileName.json', // cache : https://modmail.js.org/plugins/cache
+  }],  
+  })
+  .then(r=>{ 
+  console.log(r) // resolve ( success )
+  }).catch(err => {
+  console.log(err) // reject ( errors )
+  })
