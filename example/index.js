19d1cbdd132688c1b51834cd3f872df5539a099d
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-08-23 17:06:40 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,21 +1,26 @@
-const modmail = require('modmail.js') 
+const { Intents, on } = require('modmail.js');
 
-modmail.on({ 
-  token: 'BOT_TOKEN', 
-  inbox: 'INBOX_CHANNEL_ID', 
-  prefix: 'BOT_PREFIX', 
-  message: '@mentions/messages', 
-  inline: true, // inlineReply
+(async () => {
   
-  plugins: [{ // plugins : https://modmail.js.org/plugins
-     replyToReply: true, // replyToReply : https://modmail.js.org/plugins/replyToReply
-     cache: './FileName.json', // cache : https://modmail.js.org/plugins/cache
-  }],  
-  })
-  .then(r=>{ 
-  console.log(r) // resolve ( success )
-  }).catch(err => {
-  console.log(err) // reject ( errors )
-  })
+  
+const mail = await modmail.on({ 
+  token: 'BOT_TOKEN',
+  inbox: 'INBOX_CHANNEL_ID',
+  prefix: 'BOT_PREFIX',
+  clientIntents: [
+    Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES
+  ],
+  resolve: 'EVENTS',
+  message: '@here',
+ plugins: [{ // plugins : https://modmail.js.org/plugins
+   cache: './FileName.json', // cache : https://modmail.js.org/plugins/cache
+   replyToReply: true, // replyToReply : https://modmail.js.org/plugins/replyToReply
+   extend: true // extend : https://modmail.js.org/plugins/extend
+  }]
+  
+  });
 
-// Learn More At https://modmail.js.org
+console.log(mail);
+
+})()
+
