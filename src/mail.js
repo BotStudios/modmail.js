c17b239c2efd09c61e709d091d4eba773dd93ade
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-30 08:38:07 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -16,28 +16,6 @@ client.once('ready', async () => {
 })
 
 client.on('messageCreate', async message => {
-  if(message.author.bot)return;
-  if(message.channel.type == 'DM')return;
-  if(!message.member.roles.cache.some(role => role.id === obj.roleID))return;
-  const data = await model.findOne({ Channel: message.channel.id });
-  if(!data)return;
-  if(message.content.includes(obj.prefix+'delete'))return;
-  if(data.Channel == message.channel.id){
-      client.users.cache.get(data.User).send( `**Reply From ${message.author.username} : **\n`+`${message.content}`).then(msg => {
-       message.reply(`Successfully Send Message To <@${data.User}>`).then(m => {
-           setTimeout(() => {
-        m.delete()
-  }, 3000)
-   }).catch(err => {
-       message.react('❌');
-  })
-   message.react('✅');
-       }).catch(err => {
-       message.react('❌');    
-  })
-  }
-})
-client.on('messageCreate', async message => {
   if(message.channel.type == 'DM')return;
  if(!message.member.roles.cache.some(role => role.id === obj.roleID))return
 
